b5902d4b4485ad1549a800e906f66eaf54e7c8d3
overclear <77430138+overclear@users.noreply.github.com>
2022-01-05 11:08:05 +0700

    Add files via upload

---
+++ 1-5-2022/AboutScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+
+const AboutScreen = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            <Text>About page</Text>
+            <Button title='Go to About page again...' onPress={()=>navigation.push('About')}/>
+            <Button title='Go to Home page' onPress={() => navigation.navigate('Home')}/>
+            <Button title='Go back' onPress={() => navigation.goBack()}/>
+        </View>
+    )
+}
+
+export default AboutScreen
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
